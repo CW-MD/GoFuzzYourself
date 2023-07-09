e1b0a8fd9b2a3c6524e7d8bac0a38fb9da773d2f
CW-MD <cdayton96@gmail.com>
2023-07-08 22:46:36 -0400

    refactored read into its own func, now reads a list of urls and returns body text

--- main.go
+++ main.go
@@ -1,23 +1,94 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 )
 
 func main() {
+
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide a file")
 		os.Exit(1)
 	}
+	lines := readFile()
+	//getTest()
+	getRequest(lines)
 
+}
+
+func readFile() []string {
 	fileName := os.Args[1]
-	content, err := ioutil.ReadFile(fileName)
+
+	file, err := os.Open(fileName)
 	if err != nil {
-		fmt.Printf("Error reading file: %s\n", err)
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+	lines := make([]string, 0)
+
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+	if err := sc.Err(); err != nil {
+		fmt.Printf("Something went wrong: %s\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("File Contents: \n%s\n", content)
+	// content, err := ioutil.ReadFile(fileName)
+	// if err != nil {
+	// 	fmt.Printf("Error reading file: %s\n", err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Printf("File Contents: \n%s\n", content)
+	// getRequest(string(content))
+
+	return lines
+}
+
+func getRequest(urlArr []string) []string {
+
+	responsesBodies := []string{}
+
+	for i, url := range urlArr {
+
+		resp, err := http.Get(url)
+		if err != nil {
+			//todo fix
+			fmt.Printf("Error making a request to: %s\n", url)
+			continue
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("Something went wrong, %s\n", err)
+			continue
+		}
+		stringBody := string(body)
+		responsesBodies = append(responsesBodies, stringBody)
+		fmt.Printf("%s : %s/n", url, stringBody)
+		_ = i
+	}
+
+	return responsesBodies
+}
+
+func getTest() {
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("err0r %s", err)
+	}
+	fmt.Printf("%s", string(body))
 }
